b289dd9e6e12708eaedfee8d3ba9ed2d775336b2
sungyean <soun4618@naver.com>
2020-01-18 20:23:29 +0900

    컴포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LifeCycleSample from './LifeCycleSample'
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() *1677215).toString(16);
 }
 
+class App extends Component{
+
+  state = {
+    color : '#000000'
+    
+  }
+
+  handleClick = () => {
+    this.setState({
+      color : getRandomColor()
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick = {this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    )
+  }
+}
 export default App;
+ 
